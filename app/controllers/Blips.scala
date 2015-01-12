7433d0f5048309142e889a1af90e4069264760aa
Andrew Comminos <andrew@morlunk.com>
2015-01-12 15:45:29 -0800

    Cleaned up Play boilerplate, start hacking on Blips.

---
+++ app/controllers/Blips.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Blips extends Controller {
+
+    def list = TODO
+
+    def create = TODO
+
+    def get(id: Long) = TODO
+
+    def update(id: Long) = TODO
+
+    def delete(id: Long) = TODO
+}
