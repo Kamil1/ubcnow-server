11cfe8cd89e0df774b371d02eae67a6761777915
Kamil Khan <kamilkhan@Kamils-MacBook-Pro.local>
2015-01-18 00:27:26 -0800

    Created group and interest classes + updated documentation to relfect changes

---
+++ app/controllers/Groups.scala
@@ -0,0 +1,13 @@
+package controller
+
+import Interest.Interest
+
+class Group (_gid: Long, _name: String, _interests: Array[Interest], _concrete: Boolean) {
+  var gid = _gid
+  var name = _name
+  var interests = _interests
+  var concrete = _concrete
+
+  TODO //Methods
+}
+
---
+++ app/controllers/Interest.scala
@@ -0,0 +1,8 @@
+package Controllers
+
+class Interest (_iid: Long, _name: String) {
+  var iid = _iid
+  var name = _name
+}
+
+object Interest {TODO}
