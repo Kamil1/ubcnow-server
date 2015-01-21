f628b08e522833ef38ae9066ba3265d5f54e0a0d
Kamil Khan <kamilkhan@dhcp-128-189-251-177.ubcsecure.wireless.ubc.ca>
2015-01-20 17:54:22 -0800

    MAJOR UPDATE - added methods to classes, updated routes to reflect blip object renaming, added sample data for groups and interests in database, added PDF version of API documentation, updated group and interest fields to remove List dependancies, and other minor tweaks

--- app/controllers/Groups.scala
+++ app/controllers/Groups.scala
@@ -1,12 +1,58 @@
 package controllers
 
 import controllers.Interest
+import play.api._
+import play.api.Play.current
+import play.api.mvc._
+import play.api.db._
+import play.api.libs.json._
+import anorm._
+import play.api.libs.json.Json.JsValueWrapper
 
 case class Group(
   gid: Long,
   name: String, 
-  interests: Array[Interest],
+  interests: Long,
   concrete: Boolean
 )
 
-// TODO - METHODS
+object GroupController extends Controller {
+
+  implicit val groupWrites = new Writes[Group] {
+    def writes(group: Group): JsValue = Json.obj(
+      "gid" -> group.gid,
+      "name" -> group.name,
+      "interests" -> group.interests,
+      "concrete" -> group.concrete)
+  }
+
+  def list = Action {
+    DB.withConnection { implicit c =>
+    val results: List[Group] = SQL("select * from groups")().collect {
+      case Row(gid: Long,
+        name: String,
+        interests: Long,
+        concrete: Boolean) => Group(gid, name, interests, concrete)
+    }.toList
+    Ok(Json.toJson(results))
+  }
+}
+
+  def create = TODO
+
+  def get(gid: Long) = Action {
+    DB.withConnection { implicit c =>
+    val result: Group = SQL("select * from groups gid = {gid}").on("gid"->gid)().collect {
+      case Row(gid: Long,
+        name: String,
+        interests: Long,
+        concrete: Boolean) => Group(gid, name, interests, concrete)
+    }.head
+    Ok(Json.toJson(result))
+  }
+}
+  
+  def update(gid: Long) = TODO
+  
+  def delete(gid: Long) = TODO
+}
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,8 +3,11 @@
 # --- !Ups
 
 create table Groups (
-    id integer not null auto_increment,
-    primary key (id)
+    gid integer not null auto_increment,
+    name varchar(255) not null,
+    interest integer not null,
+    concrete Boolean not null,
+    primary key (gid)
 );
 
 create table Blips (
@@ -17,21 +20,31 @@ create table Blips (
     address varchar(255),
     lat float,
     lng float,
-    primary key (id),
+    primary key (id)
 
 --  FIXME: finish groups schema
 --  foreign key (gid) references Groups(id) 
 );
 
-insert into Groups values ();
+create table Interest (
+    iid integer not null auto_increment,
+    name varchar(255) not null,
+    primary key (iid)
+);
+
+insert into Groups (gid, name, interest, concrete)
+    values (0, 'This is a name', 0, true);
 insert into Blips (gid, title, summary, link)
     values (0, 'Blip 1 Title', 'This is a summary of a blip.', 'http://alphaproject.me');
 insert into Blips (gid, title, summary, link)
     values (0, 'Blip 2 Title', 'This is a summary of a blip.', 'http://alphaproject.me');
 insert into Blips (gid, title, summary, link)
     values (0, 'Blip 3 Title', 'This is a summary of a blip.', 'http://alphaproject.me');
+insert into Interest (iid, name)
+    values (0, 'This is a name');
 
 # --- !Downs
 
 drop table Groups;
 drop table Blips;
+drop table Interest;
