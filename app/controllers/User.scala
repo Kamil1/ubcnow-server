af0d43c6eaeca447e4d35a6945e953aafefd7e32
Kamil Khan <kamilkhan@dhcp-128-189-251-153.ubcsecure.wireless.ubc.ca>
2015-01-25 12:27:09 -0800

    bug fixes

--- app/controllers/User.scala
+++ app/controllers/User.scala
@@ -1,16 +1,37 @@
 package Controller
 
+import play.api._
+import play.api.Play.current
+import play.api.mvc._
+import play.api.db._
+import play.api.libs.json._
+import anorm._
+import play.api.libs.json.Json.JsValueWrapper
+
 case class User (
   puid: Long,
-  studentNumber: Long,
+  studentNumber: Option[Long] = None,
+  employeeNumber: Option[Long] = None,
   affliation: String,
   firstName: String,
   lastName: String,
-  interests: List[Interest],
-  groups: List[Groups]
+  interests: Long, //List[Interest]
+  groups: Long     //List[Group]
 )
 
 object UserController extends Controller {
+
+  implicit val userWrites = new Writes[User] {
+    def writes(user: User): JsValue = Json.obj(
+      "puid" -> user.puid,
+      "studentNumber" -> user.studentNumber,
+      "employeeNumber" -> user.employeeNumber,
+      "affiliation" -> user.affliation,
+      "firstName" -> user.firstName,
+      "lastName" -> user.lastName,
+      "interests" -> user.interests,
+      "groups" -> user.groups)
+  }
 
   def list = Action {
     DB.withConnection { implicit c =>
@@ -36,5 +57,17 @@ object UserController extends Controller {
   def update(puid: Long) = TODO
 
   def delete(puid: Long) = TODO
+
+  def matchUser: PartialFunction[Row,User] = {
+    case Row(puid: Long,
+      studentNumber: Option[Long],
+      employeeNumber: Option[Long],
+      affiliation: String,
+      firstName: String,
+      lastName: String,
+      interests: Long,
+      groups: Long
+      ) => User(puid, studentNumber, employeeNumber, affiliation, firstName, lastName, interests, groups)
+  }
 
 }
