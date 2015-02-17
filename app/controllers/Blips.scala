78ae5657ecd404ea0b3a3577e7368ec0d57315ee
Kamil Khan <kamil.z.khan@gmail.com>
2015-02-17 13:22:50 -0800

    Added in CAS integration, error present, onStart is not create clients

--- app/controllers/Blips.scala
+++ app/controllers/Blips.scala
@@ -6,18 +6,7 @@ import play.api.mvc._
 import play.api.db._
 import play.api.libs.json._
 import anorm._
-
-case class Blip(
-    id: Long,
-    gid: Long,
-    title: String,
-    summary: Option[String] = None,
-    link: Option[String] = None,
-    time: Option[String] = None,
-    address: Option[String] = None,
-    lat: Option[Double] = None,
-    lng: Option[Double] = None
-)
+import models.Blip
 
 object BlipController extends Controller {
 
@@ -25,7 +14,7 @@ object BlipController extends Controller {
         def writes(blip: Blip): JsValue = Json.obj(
             "id" -> blip.id,
             "gid" -> blip.gid,
-            "title" -> blip.title,
+            "name" -> blip.name,
             "summary" -> blip.summary,
             "link" -> blip.link,
             "time" -> blip.time,
@@ -36,53 +25,21 @@ object BlipController extends Controller {
 
     def list = Action {
         DB.withConnection { implicit c =>
-            val results: List[Blip] = SQL("SELECT * FROM blips")().collect {
-                case Row(id: Int,
-                  gid: Int,
-                  title: String,
-                  summary: Option[String],
-                  link: Option[String],
-                  time: Option[String],
-                  address: Option[String],
-                  lat: Option[Double],
-                  lng: Option[Double]) => Blip(id, gid, title, summary, link, time, address, lat, lng)
-            }.toList
+            val results: List[Blip] = SQL("SELECT * FROM blips")()
+                .collect(matchBlip)
+                .toList
             Ok(Json.toJson(results))
         }
     }
 
-    def create(id: Long,
-      gid: Long,
-      title: String,
-      summary: Option[String] = None,
-      link: Option[String] = None,
-      time: Option[String] = None,
-      address: Option[String] = None,
-      lat: Option[Double] = None,
-      lng: Option[Double] = None) = Action {
-        DB.withConnection { implicit c =>
-        SQL("""
-          INSERT INTO blips (id, gid, title, summary, link, time, address, lat, lng) 
-          VALUES ({id}, {gid}, {title}, {summary}, {link}, {time}, {address}, {lat}, {lng})
-          """)
-      // TODO - Match users and blips
-      }
-        
-
+    def create = TODO
 
     def get(id: Long) = Action {
         DB.withConnection { implicit c =>
-            val result: Blip = SQL("SELECT * FROM blips WHERE id = {id}").on("id" -> id)().collect {
-                case Row(id: Int,
-                  gid: Int,
-                  title: String,
-                  summary: Option[String],
-                  link: Option[String],
-                  time: Option[String],
-                  address: Option[String],
-                  lat: Option[Double],
-                  lng: Option[Double]) => Blip(id, gid, title, summary, link, time, address, lat, lng)
-            }.head
+            val result: Blip = SQL("SELECT * FROM blips WHERE id = {id}")
+                .on("id" -> id)()
+                .collect(matchBlip)
+                .head
             Ok(Json.toJson(result))
         }
     }
@@ -90,5 +47,18 @@ object BlipController extends Controller {
     def update(id: Long) = TODO
 
     def delete(id: Long) = TODO
+
+    def matchBlip: PartialFunction[Row,Blip] = {
+        case Row(id: Int,
+          gid: Int,
+          title: String,
+          summary: Option[String],
+          link: Option[String],
+          time: Option[String],
+          address: Option[String],
+          lat: Option[Double],
+          lng: Option[Double]) => Blip(id, gid, title, summary, link, time,
+                                       address, lat, lng)
+    }
 
 }
--- app/controllers/Groups.scala
+++ app/controllers/Groups.scala
@@ -7,51 +7,52 @@ import play.api.db._
 import play.api.libs.json._
 import anorm._
 import play.api.libs.json.Json.JsValueWrapper
-
-case class Group(
-  gid: Long,
-  name: String, 
-  interests: Long,
-  concrete: Boolean
-)
+import scala.collection.mutable.ArrayBuffer
+import models.Group
 
 object GroupController extends Controller {
 
-  implicit val groupWrites = new Writes[Group] {
-    def writes(group: Group): JsValue = Json.obj(
-      "gid" -> group.gid,
-      "name" -> group.name,
-      "interests" -> group.interests,
-      "concrete" -> group.concrete)
-  }
+    implicit val groupWrites = new Writes[Group] {
+      def writes(group: Group): JsValue = Json.obj(
+        "gid" -> group.gid,
+        "name" -> group.name,
+        "interests" -> group.interests,
+        "concrete" -> group.concrete)
+    }
 
-  def list = Action {
-    DB.withConnection { implicit c =>
-    val results: List[Group] = SQL("select * from groups")().collect {
-      case Row(gid: Long,
-        name: String,
-        interests: Long,
-        concrete: Boolean) => Group(gid, name, interests, concrete)
-    }.toList
-    Ok(Json.toJson(results))
-  }
-}
+    def list = Action {
+      DB.withConnection { implicit c =>
+          val results: List[Group] = SQL(
+            """SELECT * FROM groups
+            LEFT OUTER JOIN group_interests
+            ON groups.id = group_interests.gid""")()
+            .foldLeft(List[Group]()) { (base, row) =>
+                row match {
+                    case Row(gid: Int, name: String, concrete: Boolean, _, _, iid: Int) => {
+                        if (base.length > 0 && base.last.gid == gid) {
+                            // Merge interests with last group.
+                            // FIXME: a bit hackish? neat though.
+                            base.last.interests += iid
+                            base
+                        } else {
+                            Group(gid, name, concrete, ArrayBuffer(iid)) :: base
+                        }
+                    }
+                    case _ => base
+                }
+            }
+          Ok(Json.toJson(results))
+      }
+    }
+    
 
-  def create = TODO
+    def create = TODO
 
-  def get(gid: Long) = Action {
-    DB.withConnection { implicit c =>
-    val result: Group = SQL("select * from groups where gid = {gid}").on("gid"->gid)().collect {
-      case Row(gid: Long,
-        name: String,
-        interests: Long,
-        concrete: Boolean) => Group(gid, name, interests, concrete)
-    }.head
-    Ok(Json.toJson(result))
-  }
-}
-  
-  def update(gid: Long) = TODO
-  
-  def delete(gid: Long) = TODO
+    def get(gid: Long) = TODO   
+    
+    def update(gid: Long) = TODO
+    
+    def delete(gid: Long) = TODO
+
+    def search = TODO
 }
