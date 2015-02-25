fbacde90b1b2d24e913071be9d5e31b8fe223bf9
Kamil Khan <kamil.z.khan@gmail.com>
2015-02-25 11:18:04 -0800

    pre-sync commit

--- app/controllers/Global.scala
+++ app/controllers/Global.scala
@@ -16,7 +16,7 @@ object Global extends GlobalSettings {
     Future.successful(InternalServerError(
       views.html.error500.render()
     ))
-}
+  }
 
   override def onStart(app: Application) {
     Config.setErrorPage401(views.html.error401.render().toString())
@@ -25,17 +25,17 @@ object Global extends GlobalSettings {
     val baseUrl = Play.application.configuration.getString("baseUrl").get
     val casUrl = Play.application.configuration.getString("casUrl").get
 
-  // CAS
-  val casClient = new CasClient();
-  // casClient.setLogoutHandler(new PlayLogoutHandler());
-  // casClient.setCasProtocol(CasProtocol.SAML);
-  // casClient.setGateway(true);
-  /*final CasProxyReceptor casProxyReceptor = new CasProxyReceptor();
-  casProxyReceptor.setCallbackUrl("http://localhost:9000/casProxyCallback");  // Where do we take the user after they log in?
-  casClient.setCasProxyReceptor(casProxyReceptor);*/
-  casClient.setCasLoginUrl(casUrl);
+    // CAS
+    val casClient = new CasClient();
+    // casClient.setLogoutHandler(new PlayLogoutHandler());
+    // casClient.setCasProtocol(CasProtocol.SAML);
+    // casClient.setGateway(true);
+    /*final CasProxyReceptor casProxyReceptor = new CasProxyReceptor();
+    casProxyReceptor.setCallbackUrl("http://localhost:9000/casProxyCallback");  // Where do we take the user after they log in?
+    casClient.setCasProxyReceptor(casProxyReceptor);*/
+    casClient.setCasLoginUrl(casUrl);
 
-  val clients = new Clients(baseUrl + "/callback", casClient);
-  Config.setClients(clients);
-}
+    val clients = new Clients(baseUrl + "/callback", casClient);
+    Config.setClients(clients);
+  }
 }
